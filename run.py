a698558a135d92a03be80203cbd3bff39f9327c5
bevennyamande <bevennyamande@gmail.com>
2019-05-11 19:17:21 +0200

    commits

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.update({
@@ -10,24 +12,67 @@ app.config.update({
 
 # instantiate database object
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+bcrypt = Bcrypt(app)
 
 ############################# MODELS ###################################
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "users"
+    id = db.Column('user_id',db.Integer , primary_key=True)
+    age = db.Column('age',db.String(30))
+    name = db.Column('name', db.String(20), index=True)
+    password = db.Column('password' , db.String(10))
+    email = db.Column('email',db.String(50),unique=True , index=True)
+    date_joined = db.Column('date_joined' , db.DateTime)
+ 
+    def __init__(self , name ,password , email, age):
+        self.name = name
+        self.password = password
+        self.email = email
+        self.age = age
+        self.date_joined = datetime.utcnow()
+ 
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self.id)
+ 
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+############################ UTILITY FUNCTIONS #########################
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 
 ############################# ROUTES ###################################
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	if request.method == 'GET':
+	    return render_template('index.html')
+	email = request.form['email']
+	password = request.form['password']
+	# get the email and password variable from the form
+	return redirect(url_for('patient'))
 
 @app.route('/create_account')
 def create_account():
 	return ''
 
-@app.route('/patient')
+@app.route('/patient', methods=['GET', 'POST'])
 def patient():
 	'''
 	 Patient submits image, system detects the image and process
@@ -38,7 +83,8 @@ def patient():
 	 Create a counter variable to track all registered users and statistics
 
 	'''
-	return ''
+	if request.method == 'GET':
+	    return render_template('patient.html')
 
 
 
